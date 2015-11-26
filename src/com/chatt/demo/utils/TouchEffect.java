e9f3b29a2e83ed75476676e18e314765f4597109
Ryo <Ryo@Ryo-PC>
2015-11-26 23:13:58 +0700

    Class TouchEffect tạo hiệu ứng cho button

--- src/com/chatt/demo/utils/TouchEffect.java
+++ src/com/chatt/demo/utils/TouchEffect.java
@@ -5,18 +5,11 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 
-/**
- * The Class TouchEffect is the implementation of OnTouchListener interface. You
- * can apply this to views mostly Buttons to provide Touch effect and that view
- * must have a valid background. The current implementation simply set Alpha
- * value of View background.
- */
+
 public class TouchEffect implements OnTouchListener
 {
 
-	/* (non-Javadoc)
-	 * @see android.view.View.OnTouchListener#onTouch(android.view.View, android.view.MotionEvent)
-	 */
+	
 	@Override
 	public boolean onTouch(View v, MotionEvent event)
 	{
