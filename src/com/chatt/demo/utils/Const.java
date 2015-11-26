c0d252f06caf729d70a078c189b85da3ac0b07f1
Binh Bon <Binh Bon@BinhBon-PC>
2015-11-26 23:56:24 +0700

    Class Const để lưu các Hằng

--- src/com/chatt/demo/utils/Const.java
+++ src/com/chatt/demo/utils/Const.java
@@ -1,11 +1,9 @@
 package com.chatt.demo.utils;
 
-/**
- * The Class Const is a single common class to hold all the app Constants.
- */
+
 public class Const
 {
 
-	/** The Constant EXTRA_DATA. */
+	
 	public static final String EXTRA_DATA = "extraData";
 }
